31e3ddf6ca6c45cc4f8737702c1527919003832a
yuvraj <wadgeyuvraj@gmail.com>
2022-07-18 17:50:20 +0530

    First commit...

---
+++ src/git/Test.java
@@ -0,0 +1,16 @@
+package git;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if (hungry=="yes")
+			System.out.println("eat sth");
+		else{
+			
+			System.out.println("do work");
+		}
+		
+	}
+
+}
